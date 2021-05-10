25026b53281df43427c171479d1f7f706bba3dd3
Belendia Serda <belendia@gmail.com>
2021-05-10 15:01:20 +0300

    Add functionality to download org unit groups

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from dhis2 import Api
 import json
 
-SERVER = 'https://'
-USERNAME = ''
-PASSWORD = ''
+SERVER = 'https://et.dhis2.net/macepa/'
+USERNAME = 'test'
+PASSWORD = '#T0rn@d0;'
 
-ORG_UNIT_FIELDS = 'id,name,shortName,featureType,coordinates,parent'
+ORG_UNIT_FIELDS = 'id,name,shortName,featureType,coordinates,parent,openingDate'
+ORG_UNIT_GROUP_FIELDS = 'id,name,shortName,organisationUnits'
 PAGE_SIZE = 50
 
 
@@ -13,7 +14,8 @@ class DHIS2:
     def __init__(self):
         self.api = Api(SERVER, USERNAME, PASSWORD)
         self.org_units = {}
-        self.__org_unit_file_name = 'metadata/OrgUnits.json'
+        self.org_unit_groups = {}
+        self.__metadata_file_name = 'metadata/DHIS2_Metadata.json'
 
     def __download_org_units(self):
         print('Downloading org units...')
@@ -28,16 +30,35 @@ class DHIS2:
                 self.org_units['organisationUnits'] += page['organisationUnits']
             print("Page {} of {}".format(index + 1, page['pager']['pageCount']))
 
-    def __save_org_units(self):
-        print('Saving org units to {} ... '.format(self.__org_unit_file_name), end=" ")
-        del self.org_units['pager']
-        with open(self.__org_unit_file_name, 'w') as f:
-            json.dump(self.org_units, f)
+    def __download_org_units_groups(self):
+        print('Downloading org unit groups...')
+        for index, page in enumerate(
+                self.api.get_paged('organisationUnitGroups',
+                                   params={'fields': ORG_UNIT_GROUP_FIELDS},
+                                   page_size=PAGE_SIZE)):
+            if index == 0:
+                self.org_unit_groups = page
+            else:
+                # merge the new org units with the previously downloaded org units
+                self.org_unit_groups['organisationUnitGroups'] += page['organisationUnitGroups']
+            print("Page {} of {}".format(index + 1, page['pager']['pageCount']))
+
+    def __save_metadata(self):
+        print('Saving metadata to {} ... '.format(self.__metadata_file_name), end=" ")
+        if 'pager' in self.org_units:
+            del self.org_units['pager']
+        if 'pager' in self.org_unit_groups:
+            del self.org_unit_groups['pager']
+        all_metadata = {'organisationUnitGroups': self.org_unit_groups['organisationUnitGroups'],
+                        'organisationUnits': self.org_units['organisationUnits']}
+        with open(self.__metadata_file_name, 'w') as f:
+            json.dump(all_metadata, f)
         print('Done')
 
     def run(self):
         self.__download_org_units()
-        self.__save_org_units()
+        self.__download_org_units_groups()
+        self.__save_metadata()
 
 
 if __name__ == "__main__":
