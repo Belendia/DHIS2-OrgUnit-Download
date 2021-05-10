b24783cf134fcd0b38d8f6268585804c23352782
Belendia Serda <belendia@gmail.com>
2021-05-10 15:11:27 +0300

    Add feature to download org unit levels

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ PASSWORD = '#T0rn@d0;'
 
 ORG_UNIT_FIELDS = 'id,name,shortName,featureType,coordinates,parent,openingDate'
 ORG_UNIT_GROUP_FIELDS = 'id,name,shortName,organisationUnits'
+ORG_UNIT_LEVEL_FIELDS = 'id,name,level'
 PAGE_SIZE = 50
 
 
@@ -15,6 +16,7 @@ class DHIS2:
         self.api = Api(SERVER, USERNAME, PASSWORD)
         self.org_units = {}
         self.org_unit_groups = {}
+        self.org_unit_levels = {}
         self.__metadata_file_name = 'metadata/DHIS2_Metadata.json'
 
     def __download_org_units(self):
@@ -30,7 +32,7 @@ class DHIS2:
                 self.org_units['organisationUnits'] += page['organisationUnits']
             print("Page {} of {}".format(index + 1, page['pager']['pageCount']))
 
-    def __download_org_units_groups(self):
+    def __download_org_unit_groups(self):
         print('Downloading org unit groups...')
         for index, page in enumerate(
                 self.api.get_paged('organisationUnitGroups',
@@ -39,8 +41,19 @@ class DHIS2:
             if index == 0:
                 self.org_unit_groups = page
             else:
-                # merge the new org units with the previously downloaded org units
                 self.org_unit_groups['organisationUnitGroups'] += page['organisationUnitGroups']
+            print("Page {} of {}".format(index + 1, page['pager']['pageCount']))
+
+    def __download_org_unit_levels(self):
+        print('Downloading org unit levels...')
+        for index, page in enumerate(
+                self.api.get_paged('organisationUnitLevels',
+                                   params={'fields': ORG_UNIT_LEVEL_FIELDS},
+                                   page_size=PAGE_SIZE)):
+            if index == 0:
+                self.org_unit_levels = page
+            else:
+                self.org_unit_levels['organisationUnitLevels'] += page['organisationUnitLevels']
             print("Page {} of {}".format(index + 1, page['pager']['pageCount']))
 
     def __save_metadata(self):
@@ -49,15 +62,19 @@ class DHIS2:
             del self.org_units['pager']
         if 'pager' in self.org_unit_groups:
             del self.org_unit_groups['pager']
+        if 'pager' in self.org_unit_levels:
+            del self.org_unit_levels['pager']
         all_metadata = {'organisationUnitGroups': self.org_unit_groups['organisationUnitGroups'],
-                        'organisationUnits': self.org_units['organisationUnits']}
+                        'organisationUnits': self.org_units['organisationUnits'],
+                        'organisationUnitLevels': self.org_unit_levels['organisationUnitLevels']}
         with open(self.__metadata_file_name, 'w') as f:
             json.dump(all_metadata, f)
         print('Done')
 
     def run(self):
         self.__download_org_units()
-        self.__download_org_units_groups()
+        self.__download_org_unit_groups()
+        self.__download_org_unit_levels()
         self.__save_metadata()
 
 
